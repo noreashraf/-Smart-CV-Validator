bcf535bd89d9c5e0e7ba291d20e87571a17c8143
noreashraf <116683954+noreashraf@users.noreply.github.com>
2025-04-24 06:02:34 +0200

    Add files via upload

---
+++ UserControl1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace SmartCVValidator
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox1.Location = new System.Drawing.Point(154, 124);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(291, 38);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox2.Location = new System.Drawing.Point(154, 280);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(291, 38);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox3.Location = new System.Drawing.Point(154, 179);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(291, 35);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox4.Location = new System.Drawing.Point(154, 84);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(291, 34);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox5.Location = new System.Drawing.Point(154, 227);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(291, 35);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox6.Location = new System.Drawing.Point(154, 329);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(291, 37);
+            this.textBox6.TabIndex = 5;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Calibri", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(221, 387);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 37);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Validate";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Location = new System.Drawing.Point(33, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 24);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "E-mail";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(33, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 24);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(14, 238);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 24);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Phone Number";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(48, 294);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 24);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Address";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(24, 342);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(110, 24);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Postal Code";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Calisto MT", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label7.Location = new System.Drawing.Point(12, 11);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(406, 36);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Welcome to Smart CV Validator";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(626, 408);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 16);
+            this.label9.TabIndex = 15;
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(368, 408);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 16);
+            this.label10.TabIndex = 16;
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox7.Location = new System.Drawing.Point(485, 84);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(609, 282);
+            this.textBox7.TabIndex = 17;
+            this.textBox7.Text = "txtCV";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Calibri", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(85, 387);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 35);
+            this.label11.TabIndex = 20;
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Calibri", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(974, 387);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 37);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "ParseCV";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Font = new System.Drawing.Font("Calibri", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(974, 430);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 36);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "SaveToFile";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(1127, 543);
+            this.Load += new System.EventHandler(this.UserControl1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ UserControl1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Xml.Linq;
+using System.IO;
+
+namespace SmartCVValidator
+{
+    public partial class UserControl1: UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string emailPattern = @"^[\w\.-]+@[\w\.-]+\.\w+$";
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            string phonePattern = @"\+?\d{1,3}?[- .]?\(?\d{1,4}?\)?[- .]?\d{1,9}";
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            string namePattern = @"([A-Z][a-z]+(?:\s[A-Z][a-z]+)+)";
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            string passwordPattern = @"^.{6,}$";
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            string postalCodePattern = @"^\d{5}$";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // استرجاع البيانات من الحقول
+            string name = textBox4.Text;
+            string email = textBox1.Text;
+            string phone = textBox5.Text;
+            string password = textBox3.Text;
+            string address = textBox2.Text;
+            string postal = textBox6.Text;
+
+            // تحقق من صحة كل حقل باستخدام Regex
+            bool isValid = true;
+            string errorMessage = "";
+
+            // الاسم: يبدأ بحرف كبير وممكن يحتوي على مسافات
+            if (!Regex.IsMatch(name, @"^[A-Z][a-zA-Z\s]{2,}$"))
+            {
+                isValid = false;
+                errorMessage += "Invalid name.\n";
+                textBox4.BackColor = Color.MistyRose;
+            }
+            else textBox4.BackColor = Color.White;
+
+            // البريد الإلكتروني
+            if (!Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+            {
+                isValid = false;
+                errorMessage += " Invalid e-mail address.\n";
+                textBox1.BackColor = Color.MistyRose;
+            }
+            else textBox1.BackColor = Color.White;
+
+            // رقم الهاتف (مثلاً رقم مصري)
+            if (!Regex.IsMatch(phone, @"^01[0-2,5]{1}[0-9]{8}$"))
+            {
+                isValid = false;
+                errorMessage += " Invalid phone number.\n";
+                textBox5.BackColor = Color.MistyRose;
+            }
+            else textBox5.BackColor = Color.White;
+
+            // الباسورد (على الأقل 6 حروف، رقم وحرف كبير وصغير)
+            if (!Regex.IsMatch(password, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$"))
+            {
+                isValid = false;
+                errorMessage += "The password is weak.\n";
+                textBox3.BackColor = Color.MistyRose;
+            }
+            else textBox3.BackColor = Color.White;
+
+            // العنوان (نتأكد بس إنه مش فاضي)
+            if (string.IsNullOrWhiteSpace(address))
+            {
+                isValid = false;
+                errorMessage += "The address cannot be empty.\n";
+                textBox2.BackColor = Color.MistyRose;
+            }
+            else textBox2.BackColor = Color.White;
+
+            // الرمز البريدي (5 أرقام)
+            if (!Regex.IsMatch(postal, @"^\d{5}$"))
+            {
+                isValid = false;
+                errorMessage += "Invalid postal code.\n";
+                textBox6.BackColor = Color.MistyRose;
+            }
+            else textBox6.BackColor = Color.White;
+
+            // عرض النتيجة
+            if (isValid)
+            {
+                label11.ForeColor = Color.Green;
+                label11.Text = "All data is correct";
+            }
+            else
+            {
+                label11.ForeColor = Color.Red;
+                label11.Text = errorMessage;
+            }
+
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string cvText = textBox7.Text;
+
+            // استخراج الاسم الكامل (نفترض أنه سطر فيه حرف كبير في البداية لكل كلمة)
+            Match nameMatch = Regex.Match(cvText, @"(?m)^[A-Z][a-z]+\s[A-Z][a-z]+");
+
+            // استخراج البريد الإلكتروني
+            Match emailMatch = Regex.Match(cvText, @"[\w\.-]+@[\w\.-]+\.\w+");
+
+            // استخراج رقم الهاتف (نمط مصري مثلاً)
+            Match phoneMatch = Regex.Match(cvText, @"01[0-2,5]\d{8}");
+
+            // استخراج عدد سنوات الخبرة (مثلاً: "3 years", "5+ years")
+            Match experienceMatch = Regex.Match(cvText, @"\b\d+\+?\s+years?\b", RegexOptions.IgnoreCase);
+
+            // استخراج المهارات (C#, SQL, Java, Python...)
+            MatchCollection skillsMatches = Regex.Matches(cvText, @"\b(C\#|Java|Python|SQL|HTML|CSS|JavaScript|React|Node|ASP\.NET)\b", RegexOptions.IgnoreCase);
+
+            // تنسيق البيانات المستخرجة
+            string result = "";
+
+            result += nameMatch.Success ? $"👤 name : {nameMatch.Value}\n" : "The name does not exist\n";
+            result += emailMatch.Success ? $"📧 email : {emailMatch.Value}\n" : " The e-mail does not exist\n";
+            result += phoneMatch.Success ? $"📞 phone number : {phoneMatch.Value}\n" : "The phone number does not exist\n";
+            result += experienceMatch.Success ? $"experience: {experienceMatch.Value}\n" : "The number of years of experience does not exist \n";
+
+            if (skillsMatches.Count > 0)
+            {
+                var skills = string.Join(", ", skillsMatches.Cast<Match>().Select(m => m.Value));
+                result += $"🛠️ المهارات: {skills}\n";
+            }
+            else
+            {
+                result += "Skills don't exist \n";
+            }
+
+            label9.Text = result;
+            label9.ForeColor = Color.DarkBlue;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text File (*.txt)|*.txt|CSV File (*.csv)|*.csv";
+            saveFileDialog.Title = "Save CV Data";
+            saveFileDialog.FileName = "CV_Extracted_Data";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    File.WriteAllText(saveFileDialog.FileName, label9.Text);
+                    MessageBox.Show("The data was saved successfully!", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Got a mistake while saving:\n" + ex.Message, "Wrong", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
